3d36c7ac6e2253240a19271e34d56c22831853a9
Remington Arneson <antiremy@me.com>
2024-07-23 14:51:23 -0400

    Add check to change URL if path is invalid

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,10 +1,18 @@
 'use client'
  
-import React from 'react'
+import React, { useEffect } from 'react'
 import dynamic from 'next/dynamic'
  
 const App = dynamic(() => import('../App'), { ssr: false })
+
+const validRoutes = ["/", "/wrath", "/monitr"]
  
 export function Client() {
+  useEffect(() => {
+    if (!validRoutes.includes(window.location.pathname)) {
+      history.replaceState({}, "", "/")
+    }
+  }, [])
+
   return <App />
 }
